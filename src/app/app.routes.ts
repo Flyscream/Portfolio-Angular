e17fda336d15fd6b5e5327accd83b14d5fa0e415
Alexandre G. <alexgleizes18@gmail.com>
2026-01-26 14:42:58 +0100

    Probleme de page réparé + creation de la page competences et les liens entre les pages établie

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Competences } from './competences/competences';
 
 export const routes: Routes = [
-    { path: 'home', component: Home },
-    {path: '', redirectTo: 'home', pathMatch: 'full' }
-    
-
+  { path: 'home', component: Home },
+  { path: 'competences', component: Competences },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
--- src/app/competences/competences.ts
+++ src/app/competences/competences.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router'; 
 
 @Component({
   selector: 'app-competences',
-  imports: [],
+  standalone: true, 
+  imports: [RouterLink],
   templateUrl: './competences.html',
   styleUrl: './competences.css',
 })
