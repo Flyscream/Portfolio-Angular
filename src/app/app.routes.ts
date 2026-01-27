efcc456aa72fbaa9fc6caee84a7b381822fc291e
Alexandre G. <alexgleizes18@gmail.com>
2026-01-27 15:16:22 +0100

    lien de projet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Competences } from './competences/competences';
+import { Projets } from './projets/projets';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'competences', component: Competences },
+  { path: 'projets', component: Projets },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
