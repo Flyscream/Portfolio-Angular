23928171ae726f5ad870bf5f4d2e4c65a86ce01f
Alexandre G. <alexgleizes18@gmail.com>
2026-01-27 10:18:01 +0100

    La barre de navigation n'est plus statique, elle est appelée sur plusieurs pages. Les documents à télécharger ne sont plus statiques non plus : ils sont désormais appelés. Tout cela passe par la création de dossiers et de sous-dossiers qui permettent à certains éléments de ne plus être statiques.

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router'; 
+import { RouterLink } from '@angular/router';
+
+import { DocumentCardComponent } from '../shared/components/document-card/document-card'; 
 
 @Component({
   selector: 'app-home',
-  standalone: true, 
-  imports: [RouterLink], 
+  standalone: true,
+  imports: [RouterLink, DocumentCardComponent], 
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
---
+++ src/app/shared/components/document-card/document-card.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-document-card', 
+  standalone: true,
+  templateUrl: './document-card.html'
+})
+export class DocumentCardComponent {
+  @Input() title: string = '';    
+  @Input() imageSrc: string = ''; 
+}
