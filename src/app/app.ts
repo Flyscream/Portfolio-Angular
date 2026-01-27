43faf54156f619c6c0bf43497b2ba36ef32b9d3e
Alexandre G. <alexgleizes18@gmail.com>
2026-01-27 11:06:59 +0100

    optimisation de la page competences avec la creation d'un tableau de données et optimisation du footer pour qu'il ce mette sur chaque page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar'; 
+import { FooterComponent } from './shared/components/footer/footer'; // Ajoute cette ligne
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent], 
+  imports: [RouterOutlet, NavbarComponent, FooterComponent], 
   templateUrl: './app.html'
-})
+})  
 export class App {}
--- src/app/competences/competences.ts
+++ src/app/competences/competences.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router'; 
 
 @Component({
   selector: 'app-competences',
   standalone: true, 
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink], 
   templateUrl: './competences.html',
   styleUrl: './competences.css',
 })
 export class Competences {
-
+  competencesList = [
+    { title: 'Python', desc: 'IA Pacman, gestion de données, gestion de serveur.', level: 80 },
+    { title: 'HTML', desc: 'Sites internet pour des projets scolaires.', level: 75 },
+    { title: 'CSS', desc: 'Stylisation de pages, animations de pages.', level: 66 },
+    { title: 'JavaScript', desc: 'Gestion des clients, Bot Discord.js, jeux vidéo.', level: 40 },
+    { title: 'Java', desc: "Implémentation front-end et back-end d'un logiciel.", level: 75 },
+    { title: 'SQL', desc: 'Gestion et communication avec les bases de données.', level: 35 },
+    { title: 'C++', desc: 'Utilisation de C++ pour des projets scolaires.', level: 30 },
+    { title: 'VBA', desc: 'Stage de 1ère année BTS SIO.', level: 40 },
+    { title: 'PHP', desc: 'Utilisation du PHP dans mon cursus scolaire.', level: 60 },
+    { title: 'WordPress', desc: "Création d'un site vitrine.", level: 50 },
+    { title: 'Arduino', desc: 'Feux de circulation, gestion matériel et logiciel.', level: 25 },
+    { title: 'Angular', desc: 'Applications web dynamiques (Stage 2ème année).', level: 25 },
+    { title: 'VS Code', desc: 'Outil principal de développement.', level: 25 },
+    { title: 'IntelliJ IDEA', desc: 'Projets académiques et stages.', level: 25 },
+    { title: 'GitHub', desc: 'Centralisation des projets.', level: 25 }
+  ];
 }
