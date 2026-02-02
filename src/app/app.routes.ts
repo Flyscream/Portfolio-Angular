86df0ad2ad106243d975de295d57fecc035daf69
Alexandre G. <alexgleizes18@gmail.com>
2026-02-02 09:51:05 +0100

    Rajout de la page stage dans la navbar pour la navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Competences } from './competences/competences';
 import { Projets } from './projets/projets';
+import { Stage } from './stage/stage';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'competences', component: Competences },
   { path: 'projets', component: Projets },
+  { path: 'stage', component: Stage },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
+  
 ];
