7275dea98e90b4e02ef85c59e9ea3f6ec8e4ba4f
Alexandre G. <alexgleizes18@gmail.com>
2026-02-02 11:52:08 +0100

    rajout des liens pour la page contact + css tailwind pour la nav-bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Home } from './home/home';
 import { Competences } from './competences/competences';
 import { Projets } from './projets/projets';
 import { Stage } from './stage/stage';
+import { Contact } from './contact/contact';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'competences', component: Competences },
   { path: 'projets', component: Projets },
   { path: 'stage', component: Stage },
+  { path: 'contact', component: Contact },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
   
 ];
