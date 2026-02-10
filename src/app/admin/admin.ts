c256ebe51acba8566a75309bf2bd46aa84a18cff
Alexandre G. <alexgleizes18@gmail.com>
2026-02-10 09:36:51 +0100

    admin.ts pour les tickets cliens

--- src/app/admin/admin.ts
+++ src/app/admin/admin.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
 export class Admin {
+  tickets = signal<any[]>([]);
 
+  ngOnInit() {
+    this.chargerTickets();
+  }
+
+  chargerTickets() {
+    const save = localStorage.getItem('messages');
+    if (save) {
+      this.tickets.set(JSON.parse(save));
+    }
+  }
+
+  supprimerTicket(id: number) {
+    const MiseAJour = this.tickets().filter((ticket) => ticket.id !== id);
+    localStorage.setItem('messages', JSON.stringify(MiseAJour));
+    this.tickets.set(MiseAJour);
+  }
+
+  exporterENJSON() {
+    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(this.tickets()));
+    const downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", "messages_portfolio.json");
+    document.body.appendChild(downloadAnchorNode);
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  }
 }
